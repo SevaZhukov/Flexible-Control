d383f72003a1001eb0bd16d21689cf7737fa03c4
seva <mrswimmerlab@gmail.com>
2018-12-08 11:19:24 +0300

    nav

--- app/src/main/java/com/memebattle/flexible_control/MainActivity.kt
+++ app/src/main/java/com/memebattle/flexible_control/MainActivity.kt
@@ -2,11 +2,18 @@ package com.memebattle.flexible_control
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        //TODO check auth and rout to auth or main
+
     }
 }
