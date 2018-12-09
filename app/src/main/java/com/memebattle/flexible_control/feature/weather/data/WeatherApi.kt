25343a9e18439c5509ab7b05944bd4f948f31d5b
LukianBat <lukianzhukov2000@gmail.com>
2018-12-09 19:28:05 +0700

    weather

--- app/src/main/java/com/memebattle/flexible_control/feature/weather/data/WeatherApi.kt
+++ app/src/main/java/com/memebattle/flexible_control/feature/weather/data/WeatherApi.kt
@@ -1,11 +1,11 @@
 package com.memebattle.flexible_control.feature.weather.data
 
-import com.memebattle.flexible_control.feature.weather.domain.model.WeatherRequest
+import com.memebattle.flexible_control.feature.weather.domain.model.WeatherResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 
 interface WeatherApi {
         @GET("https://weather.api.here.com/weather/1.0/report.json?app_id=XMZ66PcCJqcKaYV5TQAu&app_code=xPdfiEpA7yKr_tDpWbzJjw&product=observation&name=moscow")
-        fun getWeather(): Call<WeatherRequest>
+        fun getWeather(): Call<WeatherResponse>
 }
--- app/src/main/java/com/memebattle/flexible_control/feature/weather/domain/ApiWeatherService.kt
+++ app/src/main/java/com/memebattle/flexible_control/feature/weather/domain/ApiWeatherService.kt
@@ -1,31 +1,20 @@
 package com.memebattle.flexible_control.feature.report.domain
-import android.net.Uri
-import com.google.gson.Gson
 import com.memebattle.flexible_control.core.domain.BaseCallback
-import com.memebattle.flexible_control.feature.report.data.ReportApi
-import com.memebattle.flexible_control.feature.report.domain.model.ReportRequest
 import com.memebattle.flexible_control.feature.weather.data.WeatherApi
-import com.memebattle.flexible_control.feature.weather.domain.model.WeatherRequest
-import okhttp3.MediaType
+import com.memebattle.flexible_control.feature.weather.domain.model.WeatherResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import okhttp3.ResponseBody
-import okhttp3.RequestBody
-import okhttp3.MultipartBody
-import java.io.File
-
-
 
 
 class ApiWeatherService(val weatherApi: WeatherApi) {
 
-    fun getWeather(baseCallback: BaseCallback<WeatherRequest>) {
-        weatherApi.getWeather().enqueue(object :Callback<WeatherRequest>{
-            override fun onFailure(call: Call<WeatherRequest>, t: Throwable) {
+    fun getWeather(baseCallback: BaseCallback<WeatherResponse>) {
+        weatherApi.getWeather().enqueue(object :Callback<WeatherResponse>{
+            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
                 baseCallback.onError(t)
             }
-            override fun onResponse(call: Call<WeatherRequest>, response: Response<WeatherRequest>) {
+            override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
                 baseCallback.onSuccess(response.body()!!)
             }
         })
